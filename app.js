22542518f891f3a4333853e4b4b6b22982f3bbf9
shalini <shalinibhatt6263@gmail.com>
2022-10-28 12:49:15 +0530

    Project Uploaded

---
+++ app.js
@@ -0,0 +1,370 @@
+const output = {
+  _key: "topic:1053",
+  cid: null,
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p",
+  globalTags: [],
+  lastposttime: 0,
+  learning_outcomes: [
+    "learning outcome 1",
+    "learning outcome 2",
+    "learning outcome 3",
+  ],
+  mainPid: 0,
+  postcount: 0,
+  pre_requisites: ["pre-requisites 1", "pre-requisites 2", "pre-requisites 3"],
+  project_image: "https://media.giphy.com/media/l3vR85PnGsBwu1PFK/giphy.gif",
+  short_description: "",
+  slug: "1053/new-project",
+  start_time: 1653517339588,
+  status: "published",
+  tasks: [
+    {
+      task_id: 6233,
+      task_title: "demo task",
+      task_description: "desc desc",
+      tools: [],
+      assets: [
+        {
+          asset_id: 6234,
+          asset_title: "task heading 1",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop ",
+          asset_type: "display_asset",
+          asset_content: "reflection",
+          display_asset_url: "",
+          display_asset_image: "",
+          display_asset_video: "",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop ",
+        },
+        {
+          asset_id: 6235,
+          asset_title: "task heading 2",
+          asset_description: `<div><textarea style="margin:8px;color:grey;height:230px;width: 95%;font-family: monospace, 'Arial Narrow', Arial, sans-serif;box-shadow:0px 0px 2px 2px grey; border:none;">
+         Input here...
+          </textarea><div style="float:left;margin:5px;background-color:blue;color:white;border:none;border-radius:15px;padding:10px;margin-bottom:12px">Submit</div></div>`,
+          asset_type: "input_asset",
+          asset_content: "tb",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6236,
+          asset_title: "task heading 3",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "input_asset",
+          asset_content: "reflection",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6237,
+          asset_title: "task heading 4",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url: "",
+          display_asset_image: "",
+          display_asset_video: "",
+          display_asset_docs:
+            "https://drive.google.com/file/d/1Pxw83bTKE3KkOyYnwm1u6DSVjntSrNTp/preview?usp=sharing",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6238,
+          asset_title: "task heading 5",
+          asset_description: `<iframe style="width:100%;height:400px" src="https://drive.google.com/file/d/1Pxw83bTKE3KkOyYnwm1u6DSVjntSrNTp/preview" title="W3Schools Free Online Web Tutorials">
+          </iframe>`,
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url: "",
+          display_asset_image:
+            "https://media.giphy.com/media/IzjhI7ggjDlEnMxZMu/giphy.gif",
+          display_asset_video: "",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6239,
+          asset_title: "task heading 6",
+          asset_description: `<iframe src="https://giphy.com/embed/IzjhI7ggjDlEnMxZMu" width="100%" height="372" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+          asset_type: "input_asset",
+          asset_content: "article",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6240,
+          asset_title: "task heading 7",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "input_asset",
+          asset_content: "eb",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6241,
+          asset_title: "task heading 8",
+          asset_description: `<div><iframe src="https://drive.google.com/file/d/1dBBYQsz2yt3vc4YMkRmVC3nuTBbq2GON/preview" width="100%" height="102" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+</p></div>`,
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url:
+            "https://drive.google.com/file/d/1dBBYQsz2yt3vc4YMkRmVC3nuTBbq2GON/preview?usp=sharing",
+          display_asset_image: "",
+          display_asset_video: "",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6242,
+          asset_title: "task heading 9",
+          asset_description: `<iframe width="420" loading="eager" height="315"
+src="https://www.youtube.com/embed/s8H0Sx7jy7c">
+</iframe>`,
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url: "",
+          display_asset_image: "",
+          display_asset_video: "https://www.youtube.com/embed/s8H0Sx7jy7c",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+      ],
+    },
+  ],
+  tid: 1053,
+  timestamp: 1653516431690,
+  title: "new Project",
+  tools: [],
+  type: "project",
+  uid: 100,
+  viewcount: 0,
+};
+open();
+let one = output.tasks[0].assets[0].asset_id;
+let two = output.tasks[0].assets[1].asset_id;
+let three = output.tasks[0].assets[2].asset_id;
+let four = output.tasks[0].assets[3].asset_id;
+let five = output.tasks[0].assets[4].asset_id;
+let six = output.tasks[0].assets[5].asset_id;
+let seven = output.tasks[0].assets[6].asset_id;
+let eight = output.tasks[0].assets[7].asset_id;
+let nine = output.tasks[0].assets[8].asset_id;
+console.log(one, two, three, four, five, six, seven, eight, nine);
+
+let html = "";
+output.tasks.forEach(function (element) {
+  element.assets.forEach((ele) => {
+    let title = ele.asset_title;
+    let description = ele.asset_description;
+    html += `<div
+      style="
+        width: 45%;height:fit-content;
+        margin: 20px;
+        box-shadow: 0px 0px 2px 2px grey;
+        border-radius: 5px;"
+    >
+    <h5 class="heading-one" id="heading-one" style="background-color: black;color: white;border-top-left-radius:5px;border-top-right-radius:5px; text-align: center;
+    font-family: monospace, 'Arial Narrow', Arial, sans-serif;
+    font-weight: 600;
+    font-size: 1.2rem;padding:8px">${title}</h5>
+<p style="margin:10px">${description}</p>
+   <div id="accordion-${ele.asset_id}" style="float:right;height:20px;margin:10px"><img src="image/arrow-down-sign-to-navigate.png" id="arrow-down-${ele.asset_id}" width="10px" height="10px" alt="arrowdownsign"/></div> </div>
+    `;
+  });
+});
+let htm = `<h5 id="close-button" style="text-align: center;
+font-family: monospace, 'Arial Narrow', Arial, sans-serif;
+font-weight: 600;
+font-size: 1.2rem;padding:8px">Name of the task</h5>`;
+output.tasks.forEach(function (element) {
+  element.assets.forEach((ele) => {
+    let title = ele.asset_title;
+    htm += `<li style="margin:5px">${title}</li>`;
+  });
+});
+
+document.getElementById("inside-box").innerHTML = html;
+
+function open() {
+  document.getElementById("collapse").addEventListener("click", function () {
+    document.getElementById("here").innerHTML = htm;
+    document.getElementById("asideelement").style.width = "15rem";
+    let btn = document.getElementById("collapse");
+    btn.id = "uncollapse";
+
+    closed();
+  });
+}
+function closed() {
+  document.getElementById("uncollapse").addEventListener("click", function () {
+    document.getElementById("here").innerHTML = ` <div id="here">
+      <img
+        width="40px"
+        style="float: right; margin: 5px"
+        src="image/one.png"
+        alt="one"
+      />
+    </div>`;
+
+    document.getElementById("asideelement").style.width = "5rem";
+    let btn = document.getElementById("uncollapse");
+    btn.id = "collapse";
+    open();
+  });
+}
+
+arrowtwodown();
+arrowdown();
+function arrowdown() {
+  document
+    .getElementById(`arrow-down-${one}`)
+    .addEventListener("click", function () {
+      document.getElementById(
+        `accordion-${one}`
+      ).innerHTML = `<img src="image/upload.png" id="arrow-up-${one}" width="10px" height="10px" alt="arrowdownsign"/>`;
+      document.getElementById(`accordion-${one}`).style.height = "100px";
+
+      arrowup();
+    });
+}
+function arrowup() {
+  document
+    .getElementById(`arrow-up-${one}`)
+    .addEventListener("click", function () {
+      document.getElementById(
+        `accordion-${one}`
+      ).innerHTML = `<img src="image/arrow-down-sign-to-navigate.png" id="arrow-down-${one}" width="10px" height="10px" alt="arrowdownsign"/>`;
+      document.getElementById(`accordion-${one}`).style.height = "20px";
+      arrowdown();
+    });
+}
+
+function arrowtwodown() {
+  document
+    .getElementById(`arrow-down-${two}`)
+    .addEventListener("click", function () {
+      document.getElementById(
+        `accordion-${two}`
+      ).innerHTML = `<img src="image/upload.png" id="arrow-up-${two}" width="10px" height="10px" alt="arrowdownsign"/>`;
+      document.getElementById(`accordion-${two}`).style.height = "100px";
+
+      arrowtwoup();
+    });
+}
+function arrowtwoup() {
+  document
+    .getElementById(`arrow-up-${two}`)
+    .addEventListener("click", function () {
+      document.getElementById(
+        `accordion-${two}`
+      ).innerHTML = `<img src="image/arrow-down-sign-to-navigate.png" id="arrow-down-${two}" width="10px" height="10px" alt="arrowdownsign"/>`;
+      document.getElementById(`accordion-${two}`).style.height = "20px";
+      arrowtwodown();
+    });
+}
+
+document
+  .getElementById(`arrow-down-${three}`)
+  .addEventListener("click", function () {
+    document.getElementById(
+      `accordion-${three}`
+    ).innerHTML = `<img src="image/upload.png" id="arrow-up" width="10px" height="10px" alt="arrowdownsign"/>`;
+  });
+document
+  .getElementById(`arrow-down-${four}`)
+  .addEventListener("click", function () {
+    document.getElementById(
+      `accordion-${four}`
+    ).innerHTML = `<img src="image/upload.png" id="arrow-up" width="10px" height="10px" alt="arrowdownsign"/>`;
+  });
+document
+  .getElementById(`arrow-down-${five}`)
+  .addEventListener("click", function () {
+    document.getElementById(
+      `accordion-${five}`
+    ).innerHTML = `<img src="image/upload.png" id="arrow-up" width="10px" height="10px" alt="arrowdownsign"/>`;
+  });
+document
+  .getElementById(`arrow-down-${six}`)
+  .addEventListener("click", function () {
+    document.getElementById(
+      `accordion-${six}`
+    ).innerHTML = `<img src="image/upload.png" id="arrow-up" width="10px" height="10px" alt="arrowdownsign"/>`;
+  });
+document
+  .getElementById(`arrow-down-${seven}`)
+  .addEventListener("click", function () {
+    document.getElementById(
+      `accordion-${seven}`
+    ).innerHTML = `<img src="image/upload.png" id="arrow-up" width="10px" height="10px" alt="arrowdownsign"/>`;
+  });
+document
+  .getElementById(`arrow-down-${eight}`)
+  .addEventListener("click", function () {
+    document.getElementById(
+      `accordion-${eight}`
+    ).innerHTML = `<img src="image/upload.png" id="arrow-up" width="10px" height="10px" alt="arrowdownsign"/>`;
+  });
+document
+  .getElementById(`arrow-down-${nine}`)
+  .addEventListener("click", function () {
+    document.getElementById(
+      `accordion-${nine}`
+    ).innerHTML = `<img src="image/upload.png" id="arrow-up" width="10px" height="10px" alt="arrowdownsign"/>`;
+  });
